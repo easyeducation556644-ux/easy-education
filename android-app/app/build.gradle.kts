33c98cf1072cd2af9d8b37def992c945b989ea04
easyeducation556644-ux <easyeducation556644@gmail.com>
2026-08-08 14:14:41 +0600

    Add secure WebMessage bridge dependency

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -27,5 +27,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.webkit:webkit:1.12.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
