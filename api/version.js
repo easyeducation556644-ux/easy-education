bb9b3f8a39ae4f4776369f65ff36c751c812cce5
easyeducation556644-ux <easyeducation556644@gmail.com>
2026-08-10 02:52:47 +0600

    Route read usage through version function

--- api/version.js
+++ api/version.js
@@ -1,6 +1,12 @@
+import firestoreReadUsageHandler from './_firestore-read-usage.js'
+
 const APP_VERSION = 'v9.0';
 
-export default function versionHandler(req, res) {
+export default async function versionHandler(req, res) {
+  if (req.query?.resource === 'firestore-read-usage') {
+    return firestoreReadUsageHandler(req, res)
+  }
+
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
   res.setHeader('Expires', '0');
