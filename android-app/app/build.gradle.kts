b83aab87449a619de0428c009088f35f4590a628
easyeducation556644-ux <easyeducation556644@gmail.com>
2026-08-10 08:21:18 +0600

    Bump Android version to 1.1.2

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.easyeducation.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
     }
 
     buildFeatures { buildConfig = true }
