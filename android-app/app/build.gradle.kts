86840e73e947a32ccb1ec6b5dd2d6a68443b6ba1
easyeducation556644-ux <easyeducation556644@gmail.com>
2026-08-10 16:03:29 +0600

    Bump Android release to 1.1.4

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.easyeducation.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.1.4"
     }
 
     buildFeatures { buildConfig = true }
