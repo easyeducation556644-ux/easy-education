0ef6e9bf6b77eb62d78b1837dcf210d0305584f6
easyeducation556644-ux <easyeducation556644@gmail.com>
2026-08-20 15:34:50 +0600

    Release Native Android 2.10.5

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.easyeducation.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2100400
-        versionName = "2.10.4"
+        versionCode = 2100500
+        versionName = "2.10.5"
     }
 
     buildFeatures {
