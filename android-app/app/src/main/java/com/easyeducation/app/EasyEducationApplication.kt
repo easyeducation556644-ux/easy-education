f773de99c2c1f72c88c50c264e27804176c0d157
easyeducation556644-ux <easyeducation556644@gmail.com>
2026-08-20 17:33:40 +0600

    Allow third-party cookies for embedded video players

---
+++ android-app/app/src/main/java/com/easyeducation/app/EasyEducationApplication.kt
@@ -0,0 +1,47 @@
+package com.easyeducation.app
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.CookieManager
+import android.webkit.WebView
+
+class EasyEducationApplication : Application(), Application.ActivityLifecycleCallbacks {
+    override fun onCreate() {
+        super.onCreate()
+        CookieManager.getInstance().setAcceptCookie(true)
+        registerActivityLifecycleCallbacks(this)
+    }
+
+    private fun configureEmbeddedMedia(view: View?) {
+        when (view) {
+            is WebView -> {
+                val cookies = CookieManager.getInstance()
+                cookies.setAcceptCookie(true)
+                cookies.setAcceptThirdPartyCookies(view, true)
+                view.settings.mediaPlaybackRequiresUserGesture = false
+            }
+            is ViewGroup -> {
+                for (index in 0 until view.childCount) {
+                    configureEmbeddedMedia(view.getChildAt(index))
+                }
+            }
+        }
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        configureEmbeddedMedia(activity.window?.decorView)
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        configureEmbeddedMedia(activity.window?.decorView)
+    }
+
+    override fun onActivityStarted(activity: Activity) = Unit
+    override fun onActivityPaused(activity: Activity) = Unit
+    override fun onActivityStopped(activity: Activity) = Unit
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+    override fun onActivityDestroyed(activity: Activity) = Unit
+}
