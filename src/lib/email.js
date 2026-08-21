317c6456bc63176cf470742c0aa72444bf6e5138
easyeducation556644-ux <easyeducation556644@gmail.com>
2026-08-21 20:03:31 +0600

    Create native-only Android workspace at 2.10.10

--- src/lib/email.js
+++
@@ -1,5 +0,0 @@
-export async function sendPaymentConfirmationEmail(userEmail, userName, paymentDetails) {
-  console.log('Payment confirmation email would be sent to:', userEmail)
-  console.log('Payment details:', paymentDetails)
-  return true
-}
