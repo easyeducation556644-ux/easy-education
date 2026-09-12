6dd4eff5d01379e45a908d154036c61269cdc4e1
easyeducation556644-ux <easyeducation556644@gmail.com>
2026-09-12 17:08:53 +0600

    Bump Android to 2.10.33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.easyeducation.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2103200
-        versionName = "2.10.32"
+        versionCode = 2103300
+        versionName = "2.10.33"
     }
 
     buildFeatures {
